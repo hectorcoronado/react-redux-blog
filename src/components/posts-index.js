82d2f41ea9d82824d0014a556095dd07ad8a5a34
hectorcoronado <coronado.hector@gmail.com>
2017-01-16 17:39:45 -0800

    implemented navigation bw index & posts/new pages with <Link>

--- src/components/posts-index.js
+++ src/components/posts-index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 // import { bindActionCreators } from 'redux'; // not necessary if not doing mapDispatchToProps()
 
 import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
-  /* React will automatically invoke componentWillMount whenever our component is about to be rendered to the DOM for the FIRST TIME.
+  /* React will automatically invoke componentWillMount whenever our component is about to be
+  rendered to the DOM for the FIRST TIME.
 
   It will NOT be called on subsequent renders.
   */
@@ -15,7 +17,17 @@ class PostsIndex extends Component {
 
   render() {
     return (
-      <div>List of blog posts</div>
+      <div>
+        <div className="text-xs-right">
+          {/* Link is a react-router component that works much like an HTML <a> tag, it needs a "to"
+            prop with the URL's path extension we need it to navigate to:
+          */}
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+        List of blog posts
+      </div>
     );
   }
 }
